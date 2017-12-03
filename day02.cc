49412504eaf8e10fbb1b9f43949a44f69ec199d6
Leah Neukirchen <leah@vuxu.org>
2017-12-03 17:35:02 +0100

    day02

---
+++ day02.cc
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	string line;
+	int s1 = 0, s2 = 0;
+
+	while (getline(cin, line)) {
+		istringstream is{line};
+		vector<int> v{istream_iterator<int>(is),
+		    istream_iterator<int>()};   // slurp it for part 2
+
+		auto [min, max] = minmax_element(begin(v), end(v));
+		s1 += *max - *min;
+
+		for (int a : v)
+			for (int b : v)
+				if (a > b && a % b == 0)
+					s2 += a / b;
+	}
+
+	cout << s1 << endl << s2 << endl;
+	return 0;
+}
